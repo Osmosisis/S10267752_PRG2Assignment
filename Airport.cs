aca0b8bdb072483c53413fbcc986b35a90790b2f
Osmosisis <s10267752@connect.np.edu.sg>
2025-01-18 17:29:25 +0800

    Init

---
+++ Airport.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    abstract class Flight
+    {
+		private string flightnum;
+
+		public string FlightNum
+		{
+			get { return flightnum; }
+			set { flightnum = value; }
+		}
+
+		private string origin;
+
+		public string Origin
+		{
+			get { return origin; }
+			set { origin = value; }
+		}
+
+		private string destination;
+
+		public string Destination
+		{
+			get { return destination; }
+			set { destination = value; }
+		}
+
+		private DateTime expectedtime;
+
+		public DateTime ExpectedTime
+		{
+			get { return expectedtime; }
+			set { expectedtime = value; }
+		}
+
+		private string status;
+
+		public string Status
+		{
+			get { return status; }
+			set { status = value; }
+		}
+
+		public Flight() { }
+		public Flight(string fnum, string og, string dest, DateTime et, string stat)
+		{
+			FlightNum = fnum;
+			Origin = og;
+			Destination = dest;
+			ExpectedTime = et;
+			Status = stat;
+		}
+        public abstract double CalculateFees();
+
+		public virtual string ToString()
+		{
+			return $"Flight Number: {FlightNum} \tOrigin: {Origin} \tDestination: {Destination} \tExpected Time: {ExpectedTime} \tStatus: {Status}";
+		}
+    }
+}
