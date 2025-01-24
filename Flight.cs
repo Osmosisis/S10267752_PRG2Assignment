f1e7362467dcf119b96e0a3888dfd4242cda4ae5
Osmosisis <s10267752@connect.np.edu.sg>
2025-01-24 21:30:08 +0800

    flight internal stuff

--- Flight.cs
+++ Flight.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace S10267752_PRGassignment2
 {
-    internal class Flight
+    abstract class Flight
     {
+        private string flightnum;
+
+        public string FlightNum
+        {
+            get { return flightnum; }
+            set { flightnum = value; }
+        }
+
+        private string origin;
+
+        public string Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+        public Airline airline = new Airline();
+
+        private string destination;
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        private DateTime expectedtime;
+
+        public DateTime ExpectedTime
+        {
+            get { return expectedtime; }
+            set { expectedtime = value; }
+        }
+
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Flight() { }
+        public Flight(string fnum, string og, string dest, DateTime et, string stat)
+        {
+            FlightNum = fnum;
+            Origin = og;
+            Destination = dest;
+            ExpectedTime = et;
+            Status = stat;
+        }
+        public virtual double CalculateFees();
+
+        public virtual string ToString()
+        {
+            return $"Flight Number: {FlightNum} \tOrigin: {Origin} \tDestination: {Destination} \tExpected Time: {ExpectedTime} \tStatus: {Status}";
+        }
     }
 }
