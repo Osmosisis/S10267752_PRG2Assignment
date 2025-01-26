aa8db8c6c114b5c299f590c5eda5041145991608
Yoshihiro <flopsnp@gmail.com>
2025-01-26 23:01:01 +0800

    NORM checkpoint 1

--- NORMFlight.cs
+++ NORMFlight.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
+using System.IO.IsolatedStorage;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace S10267752_PRGassignment2
 {
-    internal class NORMFlight : Flight 
+    public interface INORMFlight
+    {
+        public double CalculateFees();
+    }
+    internal class NORMFlight : Flight, INORMFlight
     {
         public NORMFlight() { }
         public NORMFlight(string fnum, string og, string dest, DateTime et, string stat) : base( fnum,  og,  dest,  et,  stat) { }
