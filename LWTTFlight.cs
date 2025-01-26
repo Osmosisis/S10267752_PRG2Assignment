c70a9e17325c48e1ad567c63fc91b0b922f76252
Yoshihiro <flopsnp@gmail.com>
2025-01-26 22:07:23 +0800

    LWTT checkpoint 1

--- LWTTFlight.cs
+++ LWTTFlight.cs
@@ -6,27 +6,26 @@ using System.Threading.Tasks;
 
 namespace S10267752_PRGassignment2
 {
-    internal class LWTTFlight : Flight 
+    public interface ILWTTFlight
     {
-        private double requestfee;
+        public double CalculateFees();
+    }
+    internal class LWTTFlight : Flight, ILWTTFlight
+    {
+        private double requestfee = 500;
 
         public double RequestFee
         {
             get { return requestfee; }
             set { requestfee = value; }
         }
-        public LWTTFlight() { }
-        public LWTTFlight(string fnum, string og, string dest, DateTime et, string stat, double requestfee) : base(fnum, og, dest, et, stat)
-        {
-            RequestFee = requestfee;
-        }
         public override double CalculateFees()
         {
-            return base.CalculateFees();
+            return requestfee;
         }
         public override string ToString()
         {
-            return base.ToString();
+            return $"{base.ToString()} \n${requestfee} requested (LWTT)";
         }
     }
 }
