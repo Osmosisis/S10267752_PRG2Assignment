d50f8e1eb6af5688eb36fea1570267c6ac3a5865
Osmosisis <s10267752@connect.np.edu.sg>
2025-02-01 23:29:19 +0800

    chnaged the capitalization

--- TerminalClass.cs
+++ TerminalClass.cs
@@ -23,9 +23,9 @@ namespace S10267752_PRGassignment2
             set { terminalname = value; }
         }
 
-        public Dictionary<string, Flight> flights { get; set; } = new Dictionary<string, Flight>();
-        public Dictionary<string, Airline> airlines { get; set; } = new Dictionary<string, Airline>();
-        public Dictionary<string, BoardingGate> boardingGates { get; set; } = new Dictionary<string, BoardingGate>();
+        public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+        public Dictionary<string, Airline> Airlines { get; set; } = new Dictionary<string, Airline>();
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; } = new Dictionary<string, BoardingGate>();
         public Dictionary<string, double> GateFees { get; set; } = new Dictionary<string, double>();
 
 
